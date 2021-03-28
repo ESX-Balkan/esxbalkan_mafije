70b76215fd42f68f0e6d01e81266e1647f4a6aed
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-03-28 11:07:14 +0100

    WiP Sef itema!

--- server/main.lua
+++ server/main.lua
@@ -306,3 +306,57 @@ ESX.RegisterServerCallback('esxbalkan_mafije:kupiOruzje', function(source, cb, w
 		end
 	end
 end)
+
+RegisterServerEvent('esxbalkan_mafije:getStockItem')
+AddEventHandler('esxbalkan_mafije:getStockItem', function(itemName, count)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+	local org = xPlayer.job.name
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_' .. org, function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+		if count > 0 and inventoryItem.count >= count then
+			if sourceItem.limit ~= -1 and (sourceItem.count + count) > sourceItem.limit then
+				TriggerClientEvent('esx:showNotification', _source, ('Nemate dovoljno prostora kod sebe da drzite toliko stvari!'))
+			else
+				inventory.removeItem(itemName, count)
+				xPlayer.addInventoryItem(itemName, count)
+				TriggerClientEvent('esx:showNotification', _source, _U('have_withdrawn', count, inventoryItem.label))
+			end
+		else
+			TriggerClientEvent('esx:showNotification', _source, _U('quantity_invalid'))
+		end
+	end)
+end)
+
+RegisterServerEvent('esxbalkan_mafije:putStockItems')
+AddEventHandler('esxbalkan_mafije:putStockItems', function(itemName, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+	local org = xPlayer.job.name
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_' .. org, function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+		-- dali igrac ima dovoljno kod sebe?
+		if sourceItem.count >= count and count > 0 then
+			xPlayer.removeInventoryItem(itemName, count)
+			inventory.addItem(itemName, count)
+			TriggerClientEvent('esx:showNotification', xPlayer.source, _U('have_deposited', count, inventoryItem.label))
+		else
+			TriggerClientEvent('esx:showNotification', xPlayer.source, _U('quantity_invalid'))
+		end
+	end)
+end)
+
+ESX.RegisterServerCallback('esxbalkan_mafije:getajsveiteme', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local org = xPlayer.job.name
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_' .. org, function(inventory)
+		cb(inventory.items)
+	end)
+end)
+
+ESX.RegisterServerCallback('esxbalkan_mafije:getajigracevinventory', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local items = xPlayer.inventory
+	cb({items = items})
+end)
