8e6b1cd81ead6e0cc2247111ba0f9c3e94d6ae0e
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-03-30 16:47:05 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config  = {}
-Config.DrawDistance = 30.0
+Config.DrawDistance = 20.0 -- 20 je savim dovoljno ne treb ni manje ni preko 20..
 Config.MarkerSize  = { x = 1.5, y = 1.5, z = 0.5 }
 Config.MarkerHelikopter   = { x = 6.0, y = 6.0, z = 2.5 }
 Config.MarkerAuto = { x = 3.0, y = 3.0, z = 3.0 }
