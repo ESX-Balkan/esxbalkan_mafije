8a77bed4074ed255f78b8a2119ae5e6c55b7fc8d
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-04-08 14:32:27 +0100

    Ovo je broken i moze nekad izbaciti error, tako da
    cemo da izbacimo to, ko oce nek fixa i napravi pull request, Hvala :)

--- client/main.lua
+++ client/main.lua
@@ -651,36 +651,6 @@ CreateThread(function()
 					end
 				end
 
-				for i=1, #Config.Mafije[jobName]['Helikopter'], 1 do
-					local distance = #(coords - Config.Mafije[jobName]['Helikopter'][i])
-
-					if distance < Config.DrawDistance then
-						if not IsPedInAnyVehicle(playerPed, false) then
-							letSleep = false
-							DrawMarker(Config.MarkerTypes.SpawnAuta, Config.Mafije[jobName]['Helikopter'][i], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, Config.MarkerColor.r, Config.MarkerColor.g, Config.MarkerColor.b, 100, false, true, 2, true, false, false, false)
-						end
-					end
-
-					if distance < Config.MarkerSize.x then
-						isInMarker, currentStation, currentPart, currentPartNum = true, k, 'Helikopter', i
-					end
-				end
-
-				for i=1, #Config.Mafije[jobName]['Brodovi'], 1 do
-					local distance = #(coords - Config.Mafije[jobName]['Brodovi'][i])
-
-					if distance < Config.DrawDistance then
-						if not IsPedInAnyVehicle(playerPed, false) then
-							letSleep = false
-							DrawMarker(Config.MarkerTypes.SpawnAuta, Config.Mafije[jobName]['Brodovi'][i], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, Config.MarkerColor.r, Config.MarkerColor.g, Config.MarkerColor.b, 100, false, true, 2, true, false, false, false)
-						end
-					end
-
-					if distance < Config.MarkerSize.x then
-						isInMarker, currentStation, currentPart, currentPartNum = true, k, 'Brodovi', i
-					end
-				end
-
 				if PlayerData.job.grade_name == 'boss' then
 					for i=1, #Config.Mafije[jobName]['BossActions'], 1 do
 						local distance = #(coords - Config.Mafije[jobName]['BossActions'][i])
