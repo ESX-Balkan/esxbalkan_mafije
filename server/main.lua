36c1b98ad19abe11ee25974f465f6d179d9a18e0
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-05-22 13:52:38 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -423,8 +423,7 @@ if GetCurrentResourceName() ~= "esxbalkan_mafije" then
 end
 
 -- Provjeri za update
-local trenutnavezija = [[1.4
-]]
+local trenutnavezija = GetResourceMetadata(GetCurrentResourceName(), "version")
 PerformHttpRequest('https://raw.githubusercontent.com/ESX-Balkan/esxbalkan_mafije/main/version', function(Error, novijaverzija, Header)
 	if trenutnavezija ~= novijaverzija then
 		print('\n')
