56575edf9661f76f7d4e46591b7c0a40d67970aa
iSentrie <xvender7@gmail.com>
2021-06-28 21:48:45 +0300

    Reworked commands for setting levels

--- server/main.lua
+++ server/main.lua
@@ -555,42 +555,22 @@ AddEventHandler('esxbalkan_mafije:updateLvL3', function(job)
 end)
 
 
-RegisterCommand('lvlup', function(source, args)
+RegisterCommand('setlvl', function(source, args)
 	job = args[1]
-	level = args[2]
+	level = tonumber(args[2])
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if source == 0 or xPlayer.getGroup() == "superadmin" then -- source == 0 kozola
 		if args[1] ~= nil and args[2] ~= nil then 
-			lvlUp(job, level, broj)	
-			print("^5 Baza ^0" ..job.. "^5 je unaprijeđena na level ^7" ..level.. "")
+			setLevel(job, level, broj)	
+			print("^5 Gang ^0" ..job.. "^5 is now level: ^7" ..level.. "")
 		end
 	else
 		TriggerClientEvent('esx:showNotification', source, ('Ne mozes koristiti ovu komandu, nisi admin!'))
 	end
 end)
 
-RegisterCommand('lvldown', function(source, args)
-	job = args[1]
-	level = args[2]
-	local xPlayer = ESX.GetPlayerFromId(source)
-	if source == 0 or xPlayer.getGroup() == "superadmin" then
-		if args[1] ~= nil and args[2] ~= nil then 
-			lvlDown(job, level, broj)	
-			print("^5 Baza ^0" ..job.. "^5 je degradirana na level ^7" ..level.. "")
-		end
-	else
-		TriggerClientEvent('esx:showNotification', source, ('Ne mozes koristiti ovu komandu, nisi admin!'))
-	end
-end)
-
-
-lvlUp = function(job, broj)
-	levelTabela[job].stats.level = levelTabela[job].stats.level + json.decode(broj)
-	saveFile(levelTabela)
-end
-
-lvlDown = function(job, broj)
-	levelTabela[job].stats.level = levelTabela[job].stats.level - json.decode(broj)
+setLevel = function(job, broj)
+	levelTabela[job].stats.level = level
 	saveFile(levelTabela)
 end
 
