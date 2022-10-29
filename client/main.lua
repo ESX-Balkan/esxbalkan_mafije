7293683ab60912747a95c5477aab2821af7eb771
Thekuca <63980591+Thekuca@users.noreply.github.com>
2022-10-29 22:15:05 +0200

    Optimizacija
    valjda radi, pozdrav

--- client/main.lua
+++ client/main.lua
@@ -574,11 +574,13 @@ AddEventHandler('esxbalkan_mafije:hasExitedMarker', function(station, part, part
 	CurrentAction = nil
 end)
 
+local breakujLoopVezivanja = true
+
 RegisterNetEvent('esxbalkan_mafije:vezivanje')
 AddEventHandler('esxbalkan_mafije:vezivanje', function()
 	isHandcuffed = not isHandcuffed
 	local playerPed = PlayerPedId()
-		if isHandcuffed then
+	if isHandcuffed then
 		RequestAnimDict('mp_arresting')
 		while not HasAnimDictLoaded('mp_arresting') do Wait(0) end
 		TaskPlayAnim(playerPed, 'mp_arresting', 'idle', 8.0, -8, -1, 49, 0, 0, 0, 0)
@@ -588,6 +590,8 @@ AddEventHandler('esxbalkan_mafije:vezivanje', function()
 		SetPedCanPlayGestureAnims(playerPed, false)
 		FreezeEntityPosition(playerPed, true)
 		DisplayRadar(false)
+		breakujLoopVezivanja = false
+		vezivanjeThread()
 	else
 		ClearPedSecondaryTask(playerPed)
 		SetEnableHandcuffs(playerPed, false)
@@ -595,6 +599,7 @@ AddEventHandler('esxbalkan_mafije:vezivanje', function()
 		SetPedCanPlayGestureAnims(playerPed, true)
 		FreezeEntityPosition(playerPed, false)
 		DisplayRadar(true)
+		breakujLoopVezivanja = true
 	end
 end)
 
@@ -609,6 +614,7 @@ AddEventHandler('esxbalkan_mafije:odvezivanje', function()
 		SetPedCanPlayGestureAnims(playerPed, true)
 		FreezeEntityPosition(playerPed, false)
 		DisplayRadar(true)
+		breakujLoopVezivanja = true
 	end
 end)
 
@@ -688,59 +694,65 @@ AddEventHandler('esxbalkan_mafije:staviVanVozila', function()
 	end
 end)
 
-CreateThread(function()
-	local DisableControlAction = DisableControlAction
-	local IsEntityPlayingAnim = IsEntityPlayingAnim
-	while 1 do
-		local Sleep = 1000
-		local playerPed = PlayerPedId()
-		if isHandcuffed then
-			Sleep = 0
-			DisableControlAction(0, 1, true) -- Disable pan
-			DisableControlAction(0, 2, true) -- Disable tilt
-			DisableControlAction(0, 24, true) -- Attack
-			DisableControlAction(0, 257, true) -- Attack 2
-			DisableControlAction(0, 25, true) -- Aim
-			DisableControlAction(0, 263, true) -- Melee Attack 1
-			DisableControlAction(0, 32, true) -- W
-			DisableControlAction(0, 34, true) -- A
-			DisableControlAction(0, 31, true) -- S
-			DisableControlAction(0, 30, true) -- D
-			DisableControlAction(0, 45, true) -- Reload
-			DisableControlAction(0, 22, true) -- Jump
-			DisableControlAction(0, 44, true) -- Cover
-			DisableControlAction(0, 37, true) -- Select Weapon
-			DisableControlAction(0, 23, true) -- Also 'enter'?
-			DisableControlAction(0, 288,  true) -- Disable phone
-			DisableControlAction(0, 289, true) -- Inventory
-			DisableControlAction(0, 170, true) -- Animations
-			DisableControlAction(0, 167, true) -- Job
-			DisableControlAction(0, 0, true) -- Disable changing view
-			DisableControlAction(0, 26, true) -- Disable looking behind
-			DisableControlAction(0, 73, true) -- Disable clearing animation
-			DisableControlAction(2, 199, true) -- Disable pause screen
-			DisableControlAction(0, 59, true) -- Disable steering in vehicle
-			DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
-			DisableControlAction(0, 72, true) -- Disable reversing in vehicle
-			DisableControlAction(2, 36, true) -- Disable going stealth
-			DisableControlAction(0, 47, true)  -- Disable weapon
-			DisableControlAction(0, 264, true) -- Disable melee
-			DisableControlAction(0, 257, true) -- Disable melee
-			DisableControlAction(0, 140, true) -- Disable melee
-			DisableControlAction(0, 141, true) -- Disable melee
-			DisableControlAction(0, 142, true) -- Disable melee
-			DisableControlAction(0, 143, true) -- Disable melee
-			DisableControlAction(0, 75, true)  -- Disable exit vehicle
-			DisableControlAction(27, 75, true) -- Disable exit vehicle
-			if not IsEntityPlayingAnim(playerPed, 'mp_arresting', 'idle', 3) then
-				ESX.Streaming.RequestAnimDict('mp_arresting', function()
-					TaskPlayAnim(playerPed, 'mp_arresting', 'idle', 8.0, -8, -1, 49, 0.0, false, false, false)
-				end)
+vezivanjeThread = function()
+	CreateThread(function()
+		local DisableControlAction = DisableControlAction
+		local IsEntityPlayingAnim = IsEntityPlayingAnim
+		while 1 do
+			local Sleep = 1000
+			local playerPed = PlayerPedId()
+			if isHandcuffed then
+				Sleep = 0
+				DisableControlAction(0, 1, true) -- Disable pan
+				DisableControlAction(0, 2, true) -- Disable tilt
+				DisableControlAction(0, 24, true) -- Attack
+				DisableControlAction(0, 257, true) -- Attack 2
+				DisableControlAction(0, 25, true) -- Aim
+				DisableControlAction(0, 263, true) -- Melee Attack 1
+				DisableControlAction(0, 32, true) -- W
+				DisableControlAction(0, 34, true) -- A
+				DisableControlAction(0, 31, true) -- S
+				DisableControlAction(0, 30, true) -- D
+				DisableControlAction(0, 45, true) -- Reload
+				DisableControlAction(0, 22, true) -- Jump
+				DisableControlAction(0, 44, true) -- Cover
+				DisableControlAction(0, 37, true) -- Select Weapon
+				DisableControlAction(0, 23, true) -- Also 'enter'?
+				DisableControlAction(0, 288,  true) -- Disable phone
+				DisableControlAction(0, 289, true) -- Inventory
+				DisableControlAction(0, 170, true) -- Animations
+				DisableControlAction(0, 167, true) -- Job
+				DisableControlAction(0, 0, true) -- Disable changing view
+				DisableControlAction(0, 26, true) -- Disable looking behind
+				DisableControlAction(0, 73, true) -- Disable clearing animation
+				DisableControlAction(2, 199, true) -- Disable pause screen
+				DisableControlAction(0, 59, true) -- Disable steering in vehicle
+				DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
+				DisableControlAction(0, 72, true) -- Disable reversing in vehicle
+				DisableControlAction(2, 36, true) -- Disable going stealth
+				DisableControlAction(0, 47, true)  -- Disable weapon
+				DisableControlAction(0, 264, true) -- Disable melee
+				DisableControlAction(0, 257, true) -- Disable melee
+				DisableControlAction(0, 140, true) -- Disable melee
+				DisableControlAction(0, 141, true) -- Disable melee
+				DisableControlAction(0, 142, true) -- Disable melee
+				DisableControlAction(0, 143, true) -- Disable melee
+				DisableControlAction(0, 75, true)  -- Disable exit vehicle
+				DisableControlAction(27, 75, true) -- Disable exit vehicle
+				if not IsEntityPlayingAnim(playerPed, 'mp_arresting', 'idle', 3) then
+					ESX.Streaming.RequestAnimDict('mp_arresting', function()
+						TaskPlayAnim(playerPed, 'mp_arresting', 'idle', 8.0, -8, -1, 49, 0.0, false, false, false)
+					end)
+				end
+			end
+			Wait(Sleep)
+			if breakujLoopVezivanja then
+				break
 			end
 		end
-		Wait(Sleep)
-	end
-end)
+	end)
+end
+	
 if not Config.Optimizacija then
 CreateThread(function()
 	local wejtara = 1000
