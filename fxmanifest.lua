b0004d288e13154bbb2815e87f887305715e196d
Tinky <48660658+Tinky124@users.noreply.github.com>
2022-12-11 12:06:55 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,17 @@
-fx_version 'cerulean'
-game 'gta5'
-name 'esxbalkan_mafije'
-author 'ESX-Balkan'
-url 'https://github.com/ESX-Balkan/esxbalkan_mafije'
-version "v2.1.6"
-lua54 'yes'
+--[[ FX Infromacije ]]--
+fx_version   'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54        'yes'
+games        { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+--[[ Skripta Infromacije ]]--
+name         'esxbalkan_mafije'
+author       'ESX-Balkan'
+version      '2.1.6'
+license      'LGPL-3.0-or-later'
+repository   'https://github.com/ESX-Balkan/esxbalkan_mafije'
+description  'Skripta za mafije koja je optimizirana najbolje i protekctovana od cheatera i najlaksa za postavljanje.'
 
 shared_scripts {
     '@es_extended/locale.lua',
